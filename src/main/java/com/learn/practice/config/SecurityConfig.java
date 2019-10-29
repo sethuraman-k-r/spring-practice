def83358aa4777428d303328147ebbbae7d10206
Sethuraman Krishnamoorthy <ramansethu605@gmail.com>
2019-10-29 14:35:24 +0530

    Custom authentication added

--- src/main/java/com/learn/practice/config/SecurityConfig.java
+++ src/main/java/com/learn/practice/config/SecurityConfig.java
@@ -1,36 +1,51 @@
 package com.learn.practice.config;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private CustomAuthProvider authProvider;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder().encode("user1")).roles("USER").and()
-				.withUser("admin").password(passwordEncoder().encode("admin")).roles("admin");
+//		auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder().encode("user1")).roles("USER").and()
+//				.withUser("admin").password(passwordEncoder().encode("admin")).roles("admin");
+		auth.authenticationProvider(authProvider);
 	}
 
 	@Override
 	public void configure(WebSecurity web) {
-		web.ignoring().antMatchers("/h2-console/**");
+		web.ignoring().antMatchers("/public/**");
+
 	}
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/resources/**").permitAll().anyRequest().authenticated().and().formLogin()
-				.loginPage("/login").permitAll().and().logout().permitAll();
+//		http.authorizeRequests().antMatchers("/resources/**").permitAll().anyRequest().authenticated().and()
+//				.formLogin().loginPage("/login").permitAll().and().logout().permitAll();
+		http.authorizeRequests().antMatchers("/home/**").hasRole("USER").and()
+		.formLogin().loginPage("/login").defaultSuccessUrl("/",true).permitAll().and().logout().permitAll();
 	}
-
+	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
---
+++ src/main/java/com/learn/practice/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.learn.practice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value = "/home", method = {RequestMethod.GET, RequestMethod.POST})
+	public ModelAndView getHome(ModelAndView modelAndView) {
+		System.out.println("Coming here");
+		modelAndView.setViewName("hello");
+		return modelAndView;
+	}
+
+}
